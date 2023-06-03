8482e2e076107addb6d30e578aaed60164cacf08
Chris Quinn <chrisquinnr@gmail.com>
2023-06-03 10:37:40 +0200

    Adding lib file

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use log::{Level, LevelFilter, Metadata, Record};
+
+pub struct RustOut;
+
+impl log::Log for RustOut {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= Level::Trace
+    }
+
+    fn log(&self, record: &Record) {
+        if self.enabled(record.metadata()) {
+            println!(
+                "[{}] - {} - {}",
+                record.level(),
+                record.target(),
+                record.args()
+            );
+        }
+    }
+
+    fn flush(&self) {}
+}
+
+/// Initializes the Rustout logger with the desired log level filter.
+pub fn init() {
+    log::set_logger(&RustOut).unwrap();
+    log::set_max_level(LevelFilter::Trace);
+}
